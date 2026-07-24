6fdb61734a1054974607897ff34f8c7ee981f3cc
CNthehuy <christopherqmee1@outlook.com>
2026-07-24 19:59:20 -0400

    Update Flask message

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Hello World"})
+    return jsonify({"message": "Deployed with Jenkins and Terraform"})
 
 @app.route("/health")
 def health():
